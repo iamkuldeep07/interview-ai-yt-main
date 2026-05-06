a8b4b6b6fc9dd732ecef42060bb9e3c63be17981
iamkuldeep07 <kuldeep45.kd@gmail.com>
2026-05-06 19:16:18 +0530

    fix

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: [
-      "http://localhost:5173",
+    //   "http://localhost:5173",
       "https://interview-ai-yt-main.vercel.app",
     ],
 
