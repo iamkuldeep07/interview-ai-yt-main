57acdeb3e10edc785a716f7fd776040d05335725
iamkuldeep07 <kuldeep45.kd@gmail.com>
2026-05-06 19:31:46 +0530

    fixing cors

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://interview-ai-backend.vercel.app",
+  baseURL: "https://interview-ai-yt-main.onrender.com",
   withCredentials: true,
 });
 
